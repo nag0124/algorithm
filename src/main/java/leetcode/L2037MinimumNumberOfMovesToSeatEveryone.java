0f3c906db613cd0bbf724056a2d4fe3da02894b9
nag0124 <wlsdnjs0124@gmail.com>
2024-06-13 14:33:48 +0900

    LeetCode 2037: Minimum Number of Moves to Seat Everyone

---
+++ src/main/java/leetcode/L2037MinimumNumberOfMovesToSeatEveryone.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class L2037MinimumNumberOfMovesToSeatEveryone {
+
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int ans = 0;
+
+        for (int i = 0; i < seats.length; i++) ans += Math.abs(seats[i] - students[i]);
+        return ans;
+    }
+
+}
