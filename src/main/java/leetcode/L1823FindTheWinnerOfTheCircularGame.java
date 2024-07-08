b37460a1ad526f4b6dfbc6d7407ec17f43e3265b
nag0124 <wlsdnjs0124@gmail.com>
2024-07-08 10:08:15 +0900

    LeetCode 1823: Find the Winner of the Circular Game

---
+++ src/main/java/leetcode/L1823FindTheWinnerOfTheCircularGame.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class L1823FindTheWinnerOfTheCircularGame {
+
+    public int findTheWinner(int n, int k) {
+        int ans = 0;
+        for (int i = 2; i <= n; i++) {
+            ans = (ans + k) % i;
+        }
+
+        return ans + 1;
+    }
+
+}
